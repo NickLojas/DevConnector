134a15dc71d666c3f9e8f9b7500a6b89c4fe0505
NickLojas <nick.lojas@mail.utoronto.ca>
2019-02-28 13:30:28 -0500

    Adding Profile page

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
@@ -38,7 +39,7 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
+    // Clear current Profile
     store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = "/login";
@@ -57,6 +58,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
